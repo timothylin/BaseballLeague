aa9c89e1426ec418d86d1ee69d29645dcc5af362
timothylin <timothywlin@gmail.com>
2015-11-18 10:27:42 -0500

    added sql script to make player team ID nullable, added properties in repo and BLL

--- BaseballLeague.BLL/BaseballOperations.cs
+++ BaseballLeague.BLL/BaseballOperations.cs
@@ -3,11 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BaseballLeague.DataLayer;
+using BaseballLeague.Models;
 
 namespace BaseballLeague.BLL
 {
     public class BaseballOperations
     {
+        private BaseballRepository _repo = new BaseballRepository();
+        private Response _response;
 
+        public BaseballOperations()
+        {
+            _response = new Response();
+        }
     }
 }
--- BaseballLeague.DataLayer/BaseballRepository.cs
+++ BaseballLeague.DataLayer/BaseballRepository.cs
@@ -3,11 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BaseballLeague.Models;
 
 namespace BaseballLeague.DataLayer
 {
     public class BaseballRepository
     {
+        public static List<League> Leagues { get; set; }
+        public static List<Team> Teams { get; set; }
+        public static List<Position> Positions { get; set; }   
+        public static List<Player> Players { get; set; }
 
+        public BaseballRepository()
+        {
+            Leagues = new List<League>();
+            Teams = new List<Team>();
+            Positions = new List<Position>();
+            Players = new List<Player>();
+        }
     }
 }
