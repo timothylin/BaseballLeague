ac84ee8b0e9d3ff881c2149485c913c7ace57803
timothylin <timothywlin@gmail.com>
2015-11-19 15:19:07 -0500

    updated insert team stored procedure, modified leagueVM

--- BaseballLeague.UI/Models/LeagueVM.cs
+++ BaseballLeague.UI/Models/LeagueVM.cs
@@ -2,19 +2,24 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 using BaseballLeague.Models;
 
 namespace BaseballLeague.UI.Models
 {
     public class LeagueVM
     {
-        public int LeagueID { get; set; }
-        public string LeagueName { get; set; }
-        public List<Team> Teams { get; set; }
+        //public int LeagueID { get; set; }
+        //public string LeagueName { get; set; }
+        //public List<Team> Teams { get; set; }
+        public List<SelectListItem> LeaguesList { get; set; }
+
+        public League League { get; set; } 
 
         public LeagueVM()
         {
-            Teams = new List<Team>();
+            LeaguesList = new List<SelectListItem>();
+            League = new League();
         }
     }
 }
