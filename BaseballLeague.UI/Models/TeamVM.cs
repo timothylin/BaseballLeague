5c86abcd37ba488155ffdfe6da0c6d4822245245
unknown <domandbillsmom@gmail.com>
2015-11-19 23:29:06 -0500

    views add team, list players, list teams

--- BaseballLeague.UI/Models/TeamVM.cs
+++ BaseballLeague.UI/Models/TeamVM.cs
@@ -2,11 +2,32 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
+using BaseballLeague.Models;
 
 namespace BaseballLeague.UI.Models
 {
     public class TeamVM
     {
+        public List<SelectListItem> LeagueList { get; set; }
+        public Team  team { get; set; }
 
+        public TeamVM()
+        {
+            LeagueList = new List<SelectListItem>();
+        }
+
+        public void CreateLeagueList(List<League> listOfLeagues)
+        {
+            foreach (var l in listOfLeagues)
+            {
+                var newItem = new SelectListItem();
+                newItem.Value = l.LeagueID.ToString();
+                newItem.Text = l.LeagueName;
+
+                LeagueList.Add(newItem);
+
+            }
+        }
     }
 }
